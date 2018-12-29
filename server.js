3fe9d7946a9f318bc94084e21efcba3d8e9a5b08
iamdylan <43369528+iamdylan@users.noreply.github.com>
2018-12-29 20:44:16 +0530

    Created a Controller for Register end-point

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const bcrypt = require('bcrypt-nodejs');
 const knex = require('knex');
+const register = require('./controllers/register');
 
 const db = knex({
     client: 'pg',
@@ -42,34 +43,9 @@ app.post('/signin', (req, res) => {
     .catch(err => res.status(400).json('Wrong Credentials'));
 })
 
-app.post('/register', (req, res)=>{
-    const { email, name, password }=req.body;
-    const hash = bcrypt.hashSync(password);
-        db.transaction(trx => {
-            trx.insert({
-                hash: hash,
-                email: email
-            })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-                return trx('users')
-                    .returning('*')
-                    .insert({
-                        email: loginEmail[0],
-                        name: name,
-                        joined: new Date()
-                }).then(user => {
-                    res.json(user[0]);
-                })
-            })
-            .then(trx.commit)
-            .catch(trx.rollback)
-        })
-        .catch(err => res.status(400).json('Unable to register'));
-})
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});
 
-app.get('/profile/:id', (req,res) => {
+app.get('/profile/:id', (req, res) => {
     const{ id } = req.params;
     db.select('*').from('users').where({id})
         .then(user => {
