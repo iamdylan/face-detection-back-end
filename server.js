6658eb5cb5cf33861b909dca643ac6cf304960b4
iamdylan <43369528+iamdylan@users.noreply.github.com>
2018-12-31 19:18:40 +0530

    Updating DB connection for Heroku Postgres

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'postgresql-closed-50703',
-        user: 'postgres',
-        password: 'root',
-        database: 'face-detection'
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
