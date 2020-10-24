2cb125dcc5ebcd7828b97d4ba66cb3e8e6aeff2d
iamdylan <43369528+iamdylan@users.noreply.github.com>
2020-10-24 19:15:51 +0530

    Switched from Heroku to AWS

--- server.js
+++ server.js
@@ -12,8 +12,11 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: true
+        host     : process.env.RDS_HOSTNAME,
+        user     : process.env.RDS_USERNAME,
+        password : process.env.RDS_PASSWORD,
+        port     : process.env.RDS_PORT,
+        database : process.env.RDS_DB_NAME
     }
 });
 
